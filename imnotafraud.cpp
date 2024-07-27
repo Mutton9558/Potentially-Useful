43ab6b3ec679b3a96be4df02d916b11ee9e81936
Mutton9558 <69904687+Mutton9558@users.noreply.github.com>
2024-07-27 22:46:22 +0800

    Create imnotafraud.cpp

---
+++ imnotafraud.cpp
@@ -0,0 +1,30 @@
+// This was my answer for the FizzBuzz test. I can code.
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    int i, j;
+
+    for(i = 1; i < 101; i++){
+        if (i % 15 == 0){
+            std::cout << "FizzBuzz";
+        }
+        else if (i % 3 == 0){
+            std::cout << "Fizz";
+        }
+        else if (i % 5 == 0){
+            std::cout << "Buzz";
+        }
+        else{
+            std::cout << i;
+        }
+        
+        if (i < 100){
+            std::cout << ", ";
+        }
+    }
+
+    return 0;
+}
